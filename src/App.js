c35ef1197f8ff2375c4f98e41c925de3e295c7d9
purusottam234 <purusottamadhikari234@gmail.com>
2022-01-25 19:50:22 +0545

    project completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import react,{Component} from 'react';
+ 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component
+{
+
+  constructor(props)
+  {
+    super(props);
+    this.state = { counter:0,
+    hasButtonBeenClicked:false
+  }
+  
+  }
+  render =() =>
+  <button onClick={this.handleClick}
+          className={this.props.className}
+          disabled={this.props.disabled ==='true' || this.props.disabled ===true}>
+            {this.props.text}{this.state.counter}
+            {this.state.hasButtonBeenClicked && <div>Button Clicked</div>}
+          </button>
+
+  handleClick =() => 
+  {
+    this.setState({counter: this.state.counter +1 },
+      ()=>this.setState({hasButtonBeenClicked:this.state.counter>0}));
+  }
+ 
 }
 
-export default App;
+
+
+ 
